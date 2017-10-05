b3ff0eeb9fe64529007fc831dac1725ec735b457
Theorclord <mikkeltheorclord@hotmail.com>
2017-10-05 09:23:24 +0200

    Copied logic from villiansLair

---
+++ Assets/Scripts/Utility/Enums.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Utility
+{
+    public enum cubeType
+    {
+        NONE,
+        WOOD,
+        GRASS,
+        ROCK
+    }
+    public class Enums
+    {
+        
+    }
+}
