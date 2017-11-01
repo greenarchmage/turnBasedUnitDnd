1cb4ec253a34590fdd8c481533136c04c1f7b2f1
Alexander Kirk Jørgensen <alkii900@msn.com>
2017-11-01 16:50:39 +0100

    Map Editor updated with different materials

---
+++ Assets/Scripts/Editor/MaterialSelection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Assets.Scripts.Utility;
+using System;
+
+public class MaterialSelection : MonoBehaviour, IPointerDownHandler {
+
+  public MapBuilder MapBuilderObject;
+  public CubeType CubeType;
+
+  public void OnPointerDown(PointerEventData eventData)
+  {
+    MapBuilderObject.ChangeCubeType(CubeType);
+  }
+}
